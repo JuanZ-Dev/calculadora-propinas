23ef49f5b0b2c9ba945c61cdfc0d6f84db7941b2
JuanZ-Dev <juan.z.981611@gmail.com>
2025-07-01 19:11:45 -0500

    add useOrder hook and OrderContents component

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -2,11 +2,15 @@ import type { MenuItem } from "../types"
 
 type MenuItemProps = {
     item: MenuItem
+    addItem: (item: MenuItem) => void
 }
 
-export default function MenuItem({item}: MenuItemProps) {
+export default function MenuItem({ item, addItem }: MenuItemProps) {
     return (
-        <button className="border-2 rounded-lg border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between cursor-pointer">
+        <button
+            className="border-2 rounded-lg border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between cursor-pointer"
+            onClick={() => addItem(item)}
+        >
             <p>{item.name}</p>
             <p className="font-black">s/{item.price}</p>
         </button>
